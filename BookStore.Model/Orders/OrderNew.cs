c2453ee69d1c00b0b13704e0803122cad26c7d34
Emo <eiv.razgrad@gmail.com>
2019-06-24 22:36:07 +0300

    refactoring OrderNew => add Address.cs. City.cs, and make NewProduct and User virtual.

--- BookStore.Model/Orders/OrderNew.cs
+++ BookStore.Model/Orders/OrderNew.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using BookStore.Model.HelpModels;
+using BookStore.Model.Address;
 
 namespace BookStore.Model
 {
@@ -13,15 +14,26 @@ namespace BookStore.Model
 
         public DateTime IssuedOn { get; set; }
 
+
+        public decimal Price { get; set; }
+
         public int Quantity { get; set; }
 
-        public NewProduct Product { get; set; }
+        public int ProductId { get; set; }
+        public virtual NewProduct Product { get; set; }
 
-        public User User { get; set; }
+        public string UserId { get; set; }
+        public virtual User User { get; set; }
+
+
+
+        public int? DeliveryAddressId { get; set; }
+        public virtual Address.Address DeliveryAddress { get; set; }
         /*  - IssuedOn (dateTime)
            - Quantity (int)
            - Product new (Product new)
            - Issuer (User)*/
 
+      
     }
 }
