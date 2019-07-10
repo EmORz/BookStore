7740e9e6aa3f2d7209991a819296155f0d659f12
Emo <eiv.razgrad@gmail.com>
2019-07-10 11:42:50 +0300

    Add UnitTest - UserServices for change first name.

---
+++ Tests/BookStore.Services.Tests/UserServicesTests.cs
@@ -0,0 +1,42 @@
+﻿using BookStore.Data;
+using BookStore.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using Xunit;
+
+namespace BookStore.Services.Tests
+{
+    public class UserServicesTests
+    {
+        [Fact]
+        public void EditFirstNameOfUserShouldChangeFirstName()
+        {
+            var options = new DbContextOptionsBuilder<BookStoreDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+
+            var dbContext = new BookStoreDbContext(options);
+
+            var testUser = new BookStoreUser()
+            {
+                UserName = "DesiUser",
+                FirstName = "Desislava"
+            };
+
+            dbContext.Users.Add(testUser);
+            dbContext.SaveChanges();
+
+            //var store = new Mock<IUserStore<BookStoreUser>>();
+          
+
+            var userServices = new UserServices(dbContext);
+            var editFirstName = "Desi";
+
+            userServices.EditFirstName(testUser, editFirstName);
+
+            Assert.Equal(editFirstName, testUser.FirstName);
+
+
+        }
+    }
+}
