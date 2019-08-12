423cf82b03464d69d0bb2eab706f33a26fba7403
Emo <eiv.razgrad@gmail.com>
2019-08-12 18:23:56 +0300

    add - Orders/Create.cshtml - view, Controllers/OrdersController.cs, ViewModels/Orders/Create/CreateOrderViewModel.cs /its not completed!/

---
+++ BookStore_Inspiration/Controllers/OrdersController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using BookStore.Services.Contracts;
+using BookStore_Inspiration.ViewModels.Orders.Create;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore_Inspiration.Controllers
+{
+    public class OrdersController : Controller
+    {
+        private readonly IUserServices _userServices;
+
+        public OrdersController(IUserServices userServices)
+        {
+            _userServices = userServices;
+        }
+        public IActionResult Create()
+        {
+            var user = this._userServices.GetUserByUsername(this.User.Identity.Name);
+
+            if (user.FirstName==null)
+            {
+                user.FirstName = "XXX";
+            }
+            if (user.LastName == null)
+            {
+                user.LastName = "XXX";
+            }
+            if (user.PhoneNumber == null)
+            {
+                user.PhoneNumber = "XXX";
+            }
+
+            var fullName = $"{user.FirstName} {user.LastName}";
+
+
+            var createOrderViewModel = new CreateOrderViewModel
+            {
+            
+                FullName = fullName,
+                PhoneNumber = user.PhoneNumber
+            };
+
+            return this.View(createOrderViewModel);
+        }
+    }
+}
---
+++ BookStore_Inspiration/ViewModels/Orders/Create/CreateOrderViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BookStore_Inspiration.ViewModels.Orders.Create
+{
+    public class CreateOrderViewModel
+    {
+        public string FullName { get; set; }
+
+        public string PhoneNumber { get; set; }
+    }
+}
